53738c7e53c4c360a39172c8fc881451428c8547
William Ryuputra <williamryuputra@yahoo.co.id>
2024-05-02 14:12:12 -0400

    Finishes Navigation Bar

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,11 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        cherry:'#F5385D'
+      }
+    },
   },
   plugins: [],
 }
