07c67a01b9c0b8da49d6bb0bd133a7371c1013cf
William Ryuputra <williamryuputra@yahoo.co.id>
2024-05-06 13:32:50 -0400

    Adds Axios and register api route

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,15 +1,40 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
+import axios from "axios"
 export default function RegisterPage(){
+    const [name,setName]=useState('');
+    const[email,setEmail]=useState('');
+    const[password,setPassword]=useState('');
+
+    // Register user handler
+    function registerUser(ev){
+        ev.preventDefault()
+        axios.post('/register', {
+            name,
+            email,
+            password,
+        })
+    }
+
     return(
         <div className="mt-4 grow flex justify-around items-center">
             {/* Login Box */}
             <div className="mb-64">
                 <h1 className="text-4xl text-center mb-4">Register</h1>
-                <form className="max-w-md mx-auto">
-                    <input type="text" placeholder="John Doe"/>
-                    <input type="email" placeholder='your@email.com' />
-                    <input type="password" placeholder="password" />
-                    <button className="login">Login</button>
+                <form className="max-w-md mx-auto" onSubmit={registerUser}>
+                    <input  type="text" 
+                            placeholder="John Doe" 
+                            value={name}
+                            onChange={ev=>setName(ev.target.value)} />
+                    <input  type="email" 
+                            placeholder='your@email.com' 
+                            value={email}
+                            onChange={ev=>setEmail(ev.target.value)}/>
+                    <input  type="password" 
+                            placeholder="password" 
+                            value={password} 
+                            onChange={ev=>setPassword(ev.target.value)}/>
+                    <button className="login">Register</button>
                     <div className="text-center py-2 text-gray-500 ">Already a member? <Link className="underline text-black" to={'/login'}>Login</Link>
                     </div>
                 </form>
