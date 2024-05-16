8b9dc2f7661f206b8441ae79cb72c0b53a797d81
William Ryuputra <williamryuputra@yahoo.co.id>
2024-05-16 13:40:31 -0400

    Finishes upload from computer

--- api/index.js
+++ api/index.js
@@ -139,8 +139,9 @@ app.post('/upload', photosMiddleware.array('photos',100) ,(req,res)=>{
         const ext = parts[parts.length-1]
         const newPath = path + '.' +ext
         fs.renameSync(path,newPath)
-        uploadedFiles.push(newPath.replace('uploads/',''))
+        uploadedFiles.push(newPath.replace('uploads\\',''))
     }
-    res.json(req.files)
+    console.log(uploadedFiles)
+    res.json(uploadedFiles)
 })
 app.listen(4000)
